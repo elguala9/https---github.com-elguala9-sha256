9ec5fe81ec1783c9d6d67592cdf8319bb3027c82
Luca Gualandi <luca.gualandi96@gmail.com>
2022-05-11 20:28:15 +0200

    from .txt to .json

--- util.ts
+++ util.ts
@@ -7,24 +7,27 @@ export class Util{
       
     public static calculateHash(dir_images : string){
 
+        var json : any= [];
+
         fs.readdirSync(dir_images).forEach((file : string) => {
             var file_content = fs.readFileSync(dir_images+file);
             var hash : string = createHash('sha256').update(file_content).digest('hex');
             console.log(hash);
-            this._writeHash(file , hash);
-                
             
-          });
-    }
+            var data = {
+                image: file,
+                square: hash
+            }
+            
+            json.push(data)
+                        
+            });
+            fs.writeFile ("./hashes.json", JSON.stringify(json), function(err : any) {
+                if (err) throw err;
+                console.log('complete');
+            });
+        }
 
-    private static _writeHash(file : string, hash : string){
 
-        var stream = fs.createWriteStream("./log.txt", {flags:'a'});
-        //console.log(new Date().toISOString());
-        [...Array(1)].forEach( function () {
-            stream.write(file + "-" + hash + "\n");
-        });
-        //console.log(new Date().toISOString());
-        stream.end();   
-    }
+    
 }
